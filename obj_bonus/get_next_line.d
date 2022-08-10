f5e176ed2a256c4f0ad1ee9d6a637eedf303bab2
Kewin Hatlas <khatlas@2-G-3.42heilbronn.de>
2022-08-10 03:53:52 +0200

    removed compiled program

--- obj_bonus/get_next_line.d
+++
@@ -1,4 +0,0 @@
-obj_bonus/get_next_line.o: helper/get_next_line.c \
-  helper/../includes/get_next_line.h
-
-helper/../includes/get_next_line.h:
--- obj_bonus/get_next_line_utils.d
+++
@@ -1,4 +0,0 @@
-obj_bonus/get_next_line_utils.o: helper/get_next_line_utils.c \
-  helper/../includes/get_next_line.h
-
-helper/../includes/get_next_line.h:
